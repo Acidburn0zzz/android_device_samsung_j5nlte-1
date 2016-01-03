532ca0ddb716083c49979387bc54b92830676f7b
Apavayan Sinha <info@apavayan.com>
2016-01-03 21:23:30 +0530

    this should be in vendors

--- proprietary/Android.mk
+++
@@ -1,9 +0,0 @@
-include $(CLEAR_VARS)
-LOCAL_MODULE := libtime_genoff
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := ../proprietary/system/vendor/lib/libtime_genoff.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_PREBUILT)
-
